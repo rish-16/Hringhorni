0e7bd19559c56f2d4853c82f0ae16288054b7d1b
rish-16 <mail.rishabh.anand@gmail.com>
2020-07-15 16:45:11 +0800

    Add spawning criteria

--- src/hringhorni.py
+++ src/hringhorni.py
@@ -10,6 +10,17 @@ class Hringhorni:
     def reset(self):
         self.grid = np.array([[0 for i in range(self.n)] for i in range(self.n)])
         
+    def raw_obs(self):
+        return self.grid
+        
+    def display(self):
+        for i in range(self.n):
+            row = ""
+            for j in range(self.n):
+                row += "{:^3}".format(self.grid[i][j])
+            print (row)
+        print ()
+                
     def render(self):
         state = plt.imshow(self.grid, animated=True)
         
@@ -25,14 +36,47 @@ class Hringhorni:
                 self.grid[x][y] = 255
                 count += 1
                 
+    # cell spawning rules
+    def judgement(self, i, j, n_neighbours):
+        if self.grid[i][j] == 0: # dead
+            if n_neighbours == 3: # reproduction
+                self.grid[i][j] = 255
+                
+        elif self.grid[i][j] == 255: # alive
+            if n_neighbours < 2: # underpopulation
+                self.grid[i][j] = 0
+            elif n_neighbours in [2, 3]: # stable population
+                self.grid[i][j] = 255 
+            elif n_neighbours > 3: # overpopulation
+                self.grid[i][j] = 0
+                
+    def get_alive_neighbours(self, i, j):
+        neighbours = [[x2-1, y2-1] for x2 in range(j-1, j+2)
+                               for y2 in range(i-1, i+2)
+                               if (-1 < j <= self.n and
+                                   -1 < i <= self.n and
+                                   (j != x2 or i != y2) and
+                                   (0 <= x2 <= self.n) and
+                                   (0 <= y2 <= self.n))]                    
+                                   
+        n_alive_neighbours = 0                                   
+        for i in range(len(neighbours)):
+            if self.grid[neighbours[i][0]][neighbours[i][1]] == 255:
+                n_alive_neighbours += 1
+                                
+        return n_alive_neighbours
+                
     def step(self):
         print ("Updating state")
-        # reset grid
-        self.reset()
-        self.random_spawn()
+        
+        for i in range(self.n):
+            for j in range(self.n):
+                n_alive_neighbours = self.get_alive_neighbours(i, j)
+                self.judgement(i, j, n_alive_neighbours)
+        
                 
     def animate(self, frames):
         fig = plt.figure()
         print ("Animating...")
         anim = animation.ArtistAnimation(fig, frames, interval=50, blit=True, repeat_delay=2000)
-        plt.show()
+        return anim
--- src/main.py
+++ src/main.py
@@ -1,13 +1,27 @@
 from hringhorni import Hringhorni
+import matplotlib.pyplot as plt
 
 grid = Hringhorni(10)
-grid.random_spawn()
-states = []
+grid.random_spawn(n_cells=10)
+T = 1
 
-# taking 10 steps
-for i in range(10):
-    state = grid.render()
+s_0 = grid.raw_obs()
+grid.display()
+
+# taking T steps
+for i in range(T):
     grid.step()
-    states.append(state)
     
-grid.animate(states)
+s_t = grid.raw_obs()
+grid.display()
+
+plt.figure(1)
+plt.subplot(121)
+plt.imshow(s_0, cmap="gray")
+plt.title("Original")
+
+plt.subplot(122)
+plt.imshow(s_t, cmap="gray")
+plt.title("T = {}".format(T))
+
+plt.show()
